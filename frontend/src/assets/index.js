84d44d83f8c580ba077de86ec15643101ab54df4
BounceSensei <robinson.domingo11@gmail.com>
2025-10-22 23:59:34 +0800

    remove dots for the paths

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,10 +1,10 @@
-import WebLogo from './WebLogo.png';
-import BusPicture from './buspicture.jpeg';
-import Noran from './team/noran.png';
-import DeLaPena from './team/Jansen.jpg';
-import Ladrera from './team/jose.jpg';
-import Sediaco from './team/jay.jpg';
-import Domingo from './team/RobinPic.jpg';
+import WebLogo from '/WebLogo.png';
+import BusPicture from '/buspicture.jpeg';
+import Noran from '/team/noran.png';
+import DeLaPena from '/team/Jansen.jpg';
+import Ladrera from '/team/jose.jpg';
+import Sediaco from '/team/jay.jpg';
+import Domingo from '/team/RobinPic.jpg';
 
 export {
     WebLogo,
